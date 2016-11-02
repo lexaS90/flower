4a6371ef696206ebe7b463fdacd7a851f59c707d
Alexy Stupakov <workinweb24@gmail.com>
2016-11-02 23:10:45 +0200

    Секция приемущества

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(document ).ready(function(){
-
+	$(document).ready(function(){
+  	$(".main-slider").owlCarousel({
+  		navigation : true,
+		 	navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		 	singleItem : true, 
+		 	autoPlay:3000,
+  	});
+	});
 });
 
 $(window).scroll(function(){
