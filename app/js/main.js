d2fba22b805e65d85233663248a7e50aa582e5b5
Alexy Stupakov <workinweb24@gmail.com>
2016-11-07 18:21:14 +0200

    Каталог

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,132 @@
 $(document ).ready(function(){
-	$(document).ready(function(){
-  	$(".main-slider").owlCarousel({
-  		navigation : true,
-		 	navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		 	singleItem : true, 
-		 	autoPlay:3000,
-  	});
+
+  /*
+   *  Slider
+   */
+
+	$(".main-slider").owlCarousel({
+		navigation : true,
+	 	navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	 	singleItem : true, 
+	 	autoPlay:3000,
 	});
+
+  /*
+   *  Product filter
+   */
+   $(function(){
+    $('#product_filter').mixItUp({
+      selectors: {
+        target: '.product',
+        filter: '.filter'
+      },
+      callbacks: {
+
+        onMixStart: function(){
+          waypoint2.disable();          
+        },
+        onMixEnd: function(){
+          $('.product__wrap').equalHeights();
+          //waypoint2.refresh();
+          waypoint2.enable();
+        },
+      }
+    });
+  });
+
+
+  /*
+   *  Phone
+   */
+   $(".phone").mask("(999) 999-9999");
+
+
 });
 
-$(window).scroll(function(){
+// $(window).scroll(function(){
+// });
 
+ 
+/*
+ *  Map
+ */
+
+/*var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.678698, lng: 33.924012},
+    zoom: 17
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 51.678698, lng: 33.924012},
+    map: map,
+    title: 'Hello World!'
+  });
+}*/
+
+/*
+ *  End Map
+ */
+
+/*
+ *  Checkout fixed
+ */
+
+
+var  top_checkout;
+
+var waypoint = new Waypoint({
+  element: $('#checkout')[0],
+  handler: function(direction) {
+    top_checkout = $(this.element).offset().top;
+    console.log(top_checkout);
+    var arg = [];
+    arg['el'] = $(this.element);
+    arg['top_checkout'] = top_checkout;
+
+    if (direction == 'down'){
+      $(window).scroll(arg,checkout_fix);
+    }
+    if(direction == 'up'){
+      $(window).off("scroll", checkout_fix);
+      $(this.element).css('margin-top', 0);
+    }
+  }
 });
+
+var h = $('#checkout').outerHeight()+20;
+
+var waypoint2 = new Waypoint({
+  element: $('.page__advantages')[0],
+  handler: function(direction) {
+    var arg = [];
+    arg['el'] = $('#checkout');
+    arg['top_checkout'] = top_checkout;
+
+    if (direction == 'down'){
+      $(window).off("scroll", checkout_fix);
+    }
+    if(direction == 'up'){
+      $(window).scroll(arg,checkout_fix);
+    }
+
+  },
+  offset: h+'px',
+});
+
+function checkout_fix(param){
+
+  var el = param.data['el'];
+  var top_checkout = param.data['top_checkout'];
+
+  var top_window = $(window).scrollTop();
+
+  var margin_checkout = top_window-top_checkout;
+
+  el.css('margin-top', margin_checkout+'px');
+
+}
+
+
+
